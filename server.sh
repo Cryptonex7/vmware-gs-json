b810cd8abd1acd3406de4e2631ae96aaf9264d36
Anujeet Chatterjee <anujeetc@vmware.com>
2021-09-22 18:14:03 +0530

    scoring and script mproveents

--- server.sh
+++ server.sh
@@ -1,10 +1,14 @@
+echo "Killing running task:"
+
+pm2 del logo-pro
+
 echo "Pulling from git:"
 
 git stash
 git pull 
 
 echo "Building the App:"
-
+npm i
 npm run build
 
 echo "Starting UI Server at Port 80:"
